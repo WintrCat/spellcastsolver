0ee69b50fb53456610e0246c595107e65d215df7
WintrCat <wintrcat.wilson@gmail.com>
2024-08-19 17:56:05 +0100

    feat: update dictionary, fix top move ordering bug
    updated the dictionary with more words, and fixed a small bug where moves with the same estimated long term value would be put in the wrong order. they are now properly ordered by the number of gems they collect if they have the same estimated value

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from time import time
 from json import load
+from functools import cmp_to_key
 from src.spellcast import Spellcast
+from src.searchnode import SearchNode
 
 start_time = time()
 
@@ -11,16 +13,27 @@ game.load_file("board.txt")
 
 print("searching for moves...")
 
-best_moves = game.legal_moves(lambda node: (
-    node.estimated_long_term_score(game)
+def compare_moves(a: SearchNode, b: SearchNode):
+    a_score = a.estimated_long_term_score(game)
+    b_score = b.estimated_long_term_score(game)
+
+    difference = a_score - b_score
+    if difference == 0:
+        return a.gem_count() - b.gem_count()
+    else:
+        return difference
+
+
+best_moves = game.legal_moves(
+    cmp_to_key(compare_moves)
     if config["gemManagement"]
-    else node.score()
-))
+    else SearchNode.score
+)
 
 shuffle_score, shuffle_recommended = game.evaluate_shuffle(best_moves[0])
 
 if shuffle_recommended:
-    print(f"engine recommends shuffling - {shuffle_score} estimated points")
+    print(f"engine recommends shuffling - {shuffle_score} estimated value")
 
 print("\n".join([
     f"{i + 1} > {node.to_string(game)}"
